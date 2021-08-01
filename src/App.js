576d874264d62c09bffde742b834fa8e0a2de339
Abel Souza <abelsouzacosta@gmail.com>
2021-08-01 06:17:36 -0300

    transoformando a tabela em um componente de Classe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <div>
-      <h1>Tabela de Livros</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import TabelaHead from "./components/TabelaHead";
+import TabelaBody from "./components/TabelaBody";
+import TabelaFoot from "./components/TabelaFoot";
+
+class App extends Component {
+  render() {
+    return (
+      <table className="tabela">
+        <TabelaHead />
+        <TabelaBody />
+        <TabelaFoot />
+      </table>
+    );
+  }
 }
 
 export default App;
