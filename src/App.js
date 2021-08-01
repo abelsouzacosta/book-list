03f860b483f960d2faec601f9567e96d5e815b0f
Abel Souza <abelsouzacosta@gmail.com>
2021-08-01 06:21:38 -0300

    Adicionando estado da tabela de livros

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import TabelaBody from "./components/TabelaBody";
 import TabelaFoot from "./components/TabelaFoot";
 
 class App extends Component {
+  state = {
+    livros: [
+      {
+        id: "978-85-7522-632-2",
+        titulo: "Css Grid Layout",
+        autor: "Maurício Samy Silva",
+      },
+      {
+        id: "978-85-7522-677-3",
+        titulo: "Node Essencial",
+        autor: "Ricardo R. Lecheta",
+      },
+      {
+        id: "978-85-7522-512-7",
+        titulo: "Aprendendo Material Design",
+        autor: "Kyle Mew",
+      },
+    ],
+  };
+
   render() {
     return (
       <table className="tabela">
